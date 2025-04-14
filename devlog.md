89520ee3c438f2398ee89fd4e383575cbadf0fe1
David Vu <47286500+SushiiSlobby@users.noreply.github.com>
2025-04-13 21:16:34 -0500

    implement customer and teller classes; append devlog.md

--- devlog.md
+++ devlog.md
@@ -10,5 +10,9 @@ we'll see.
 
 4-13-2025 @ 16:00
 I had to reformat the project to be contained in a single file, as per the project specifications. No bottlenecks as of
-yet while I inplement the run functions for each thread, but I do anticipate them coming up as soon as the first run is
+yet while I implement the run functions for each thread, but I do anticipate them coming up as soon as the first run is
 done.
+
+4-13-2025 @ 21:00
+The main backbone of the project is complete, now it is just testing and bugfixing. I had to reformat some of the code
+since it didn't follow the expected output, but most inputs should be covered within the classes. 
--- src/Bank_Simulation.java
+++ src/Bank_Simulation.java
@@ -52,7 +52,45 @@ public class Bank_Simulation {
 
         @Override
         public void run() {
-            System.out.printf("Teller %d [");
+            System.out.printf("Teller %d []: Ready to serve\n",id, id);
+
+            while(customersServed.get() < NUM_CUSTOMERS) {
+                try {
+                    System.out.printf("Teller %d [] Waiting for a customer\n", id);
+                    currentCustomer = customerQueue.take();
+
+                    System.out.printf("Teller %d [Customer %d]: Customer selected\n", id, currentCustomer.id);
+                    customerReady.release();
+                    currentCustomer.transactionTypeAvailable.acquire();
+
+                    System.out.printf("Teller %d [Customer %d]: serving a customer\n", id, currentCustomer.id);
+                    System.out.printf("Teller %d [Customer %d]: asks for transaction\n", id, currentCustomer.id);
+
+                    if(currentCustomer.isWithdrawal) {
+                        System.out.printf("Teller %d [Customer %d]: Handling withdrawal transaction\n", id, currentCustomer.id);
+                        System.out.printf("Teller %d [Customer %d]: Going to manager\n", id, currentCustomer.id);
+                        managerAccess.acquire();
+                        System.out.printf("Teller %d [Customer %d]: Talking to manager\n", id, currentCustomer.id);
+                        Thread.sleep(random(5,30));
+                        managerAccess.release();
+                    }
+
+                    System.out.printf("Teller %d [Customer %d]: Going to safe\n", id, currentCustomer.id);
+                    safeAccess.acquire();
+                    System.out.printf("Teller %d [Customer %d]: In safe\n", id, currentCustomer.id);
+                    Thread.sleep(random(10,50));
+                    System.out.printf("Teller %d [Customer %d]: Leaving safe\n", id, currentCustomer.id);
+                    safeAccess.release();
+
+                    System.out.printf("Teller %d [Customer %d]: Transaction complete\n", id, currentCustomer.id);
+                    transactionDone.release();
+                    currentCustomer.left.acquire();
+
+                    customersServed.incrementAndGet();
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
         }
     }
 
@@ -69,6 +107,37 @@ public class Bank_Simulation {
 
         @Override
         public void run() {
+            try{
+                Thread.sleep(random(0,100));
+                System.out.printf("Customer %d []: Wants to perform %s transaction\n", id, isWithdrawal ? "withdrawal" : "deposit");
+
+                bankOpen.acquire();
+
+                System.out.printf("Customer %d []: Going to bank\n", id);
+                doorAccess.acquire();
+                System.out.printf("Customer %d []: Entering bank\n", id);
+                System.out.printf("Customer %d []: Getting in line\n", id);
+                System.out.printf("Customer %d []: Selecting a teller\n", id);
+
+                Teller teller = getNextTeller();
+                System.out.printf("Customer %d [Teller %d]: Selected teller\n", id, teller.id);
+                System.out.printf("Customer %d [Teller %d]: Introduces self\n", id, teller.id);
+
+                customerQueue.put(this);
+                teller.customerReady.acquire();
+
+                System.out.printf("Customer %d [Teller %d]: asks for %s transaction\n", id, teller.id, isWithdrawal ? "withdrawal" : "deposit");
+
+                transactionTypeAvailable.release();
+                teller.transactionDone.acquire();
+
+                System.out.printf("Customer %d [Teller %d]: Leaving after transaction\n", id, teller.id);
+                left.release();
+                doorAccess.release();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
@@ -77,7 +146,7 @@ public class Bank_Simulation {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 
-    private static synchronized Teller findAvailableTeller() {
+    private static synchronized Teller getNextTeller() {
         Teller teller = tellers.get(tellerIndex);
         tellerIndex = (tellerIndex + 1) % tellers.size();
         return teller;
